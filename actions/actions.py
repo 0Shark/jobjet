f897408ec8057425ea049715545992eb245b883c
Elmer Dema <elmerdema2022@gmail.com>
2023-11-29 20:56:42 +0100

    changes to yml files

--- actions/actions.py
+++ actions/actions.py
@@ -13,29 +13,30 @@ class ActionRegisterLogin(Action):
     def name(self) -> Text:
         return "action_register_login"
     
-    def run(self, dispatcher:CollectingDispatcher, tracker:Tracker, domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-        username=tracker.get_slot("username")
-        password=tracker.get_slot("password")
-        roles=tracker.get_slot("roles")
+    def run(self, dispatcher: CollectingDispatcher, tracker: Tracker, domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+        username = tracker.get_slot("username")
+        password = tracker.get_slot("password")
+        roles = tracker.get_slot("roles")
 
-        register_url="http://localhost:5000/register"
-        login_url="http://localhost:5000/login"
-        register_data={"username":username, "password":password, "roles":roles}
-        register_response= requests.post(register_url, json=register_data).json()
+        register_url = "http://localhost:5000/register"
+        login_url = "http://localhost:5000/login"
 
+        # Register user
+        register_data = {"username": username, "password": password, "roles": roles}
+        register_response = requests.post(register_url, json=register_data).json()
 
-        if register_response.status_code==201:
+        if register_response["status"] == "success":
             dispatcher.utter_message(text="User created")
         else:
-            dispatcher.utter_message(text="Registration failed")
+            dispatcher.utter_message(text=f"Registration failed. Reason: {register_response.get('message', 'Unknown error')}")
 
-        login_url="http://localhost:5000/login"
-        login_data={"username":username, "password":password, "roles":roles}
-        login_response= requests.post(login_url, json=login_data).json()
+        # Login user
+        login_data = {"username": username, "password": password, "roles": roles}
+        login_response = requests.post(login_url, json=login_data).json()
 
-        if login_response.status_code==200:
+        if login_response["status"] == "success":
             dispatcher.utter_message(text="Login successful")
         else:
-            dispatcher.utter_message(text="Login failed")
-        
+            dispatcher.utter_message(text=f"Login failed. Reason: {login_response.get('message', 'Unknown error')}")
+
         return []
